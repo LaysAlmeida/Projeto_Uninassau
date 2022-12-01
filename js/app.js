f9b963909ed4d344a05a5614764ad4e4afaf55f2
Ramon Gabriel <ramongtstudy@gmail.com>
2022-12-01 14:08:46 -0300

    Melhora no Js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
+const oculta = document.getElementById("oculta")
+oculta.style.display = "none"
+const resultabusca = document.getElementsByTagName("h2")
+
 const procura = () => {
     const searchbox = document.getElementById("buscacuidador").value.toUpperCase();
     const vagas = document.getElementById("professional-list")
     const enfermeira = document.querySelectorAll(".enfermeira")
-    const pname = document.getElementsByTagName("h4")
+    const pname = vagas.getElementsByTagName("h4")
+    
 
     for(var i = 0; i < pname.length; i++) {
         let achou = enfermeira[i].getElementsByTagName("h4")[0];
 
         if(achou) {
             let textvalue = achou.textContent || achou.innerHTML
-
+            
             if(textvalue.toUpperCase().indexOf(searchbox) > - 1) {
                 enfermeira[i].style.display = "";
+                oculta.style.display = ""
             } else {
                 enfermeira[i].style.display = "none";
+                oculta.style.display = ""
             }
         }
     }
